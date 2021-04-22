e319494c0a88343fff5cfb5f2621aa1c930ab9e3
Seth Blanchard <nsethblanchard@gmail.com>
2021-04-22 14:21:25 -0400

    commit tree f7518f5f0da297be9586537c604b313299668e39

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+
+function findMatching(drivers, string) { 
+    let list = drivers.filter(driver => {
+        let lower = string.toLowerCase();
+        // added lower to catch the case-insensitive ones
+        return driver === string || driver === lower;
+    })
+    return list
+}
+
+
+
+
+function fuzzyMatch(drivers, letters) {
+    let list = drivers.filter(driver => {
+        return driver[0] === letters[0] && driver[1] === letters[1]
+    })
+    return list
+}
+
+function matchName(drivers, string) {
+    console.log(drivers)
+    console.log(string)
+
+    let list = drivers.filter(driver => {
+        return driver.name === string
+    })
+    return list;
+}
